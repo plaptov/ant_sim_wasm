91d5fd4a77bf885d2f3fe8753503bed87f404ce2
Pavel Laptov <pavel.laptov@ya.ru>
2020-07-12 22:08:06 +0300

    Return iterator instead or array

--- www/system/struct-array-iterator.ts
+++ www/system/struct-array-iterator.ts
@@ -4,16 +4,47 @@ export function iter<T extends ByteSized, V extends InstanceType<T>>(
     type: T,
     pointer: number,
     elementsCount: number
-) : V[] {
-    let result: V[] = [];
-    const cellSizeInBytes = type.size_in_bytes();
-    let curPtr = pointer - 4; // why -4? i don't know
-    for (let x = 0; x < elementsCount; x++) {
-        const obj = Object.create(type.prototype);
-        obj.ptr = curPtr;
-        const cell = obj as V;
-        result.push(cell);
-        curPtr += cellSizeInBytes;
+) : StructArrayIterator<V> {
+    return new StructArrayIterator<V>(
+        pointer - 4,
+        type.prototype,
+        type.size_in_bytes(),
+        elementsCount
+    );
+}
+
+export class StructArrayIterator<T> implements IterableIterator<T>
+{
+    constructor(
+        private pointer: number,
+        private readonly prototype: object,
+        private readonly sizeInBytes: number,
+        elementsCount: number)
+    {
+        this.endAddress = pointer + elementsCount * sizeInBytes;
     }
-    return result;
+
+    private readonly endAddress: number;
+
+    [Symbol.iterator](): IterableIterator<T> {
+        return this;
+    }
+
+    public next() : IteratorResult<T> {
+        if (this.pointer < this.endAddress) {
+            const obj = Object.create(this.prototype);
+            obj.ptr = this.pointer;
+            this.pointer += this.sizeInBytes;
+            return {
+                value: obj as T,
+                done: false,
+            };
+        } else {
+            return {
+                value: null,
+                done: true,
+            };
+        }
+    }
+
 }
